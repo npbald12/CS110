e9fcaa640ae842bc9d0f3bb6baafb64bb60f3704
Nic <Nic@140.190.159.17>
2013-01-17 09:01:05 -0600

    Hello World from LapTop

---
+++ cs110/src/assignments/HelloWorld.java
@@ -0,0 +1,32 @@
+package assignments;
+
+public class HelloWorld {
+	public static void main(String[] arg) {
+		int y;
+		y = 10;
+		int z = y + 50;
+		double d = 100.0;
+		float f = 50.0F;
+		long l = 100;
+	
+		char c = 'c';
+		boolean o = true; 
+		
+		short sh = 3;
+		
+		
+		
+		System.out.println("Hello World");
+		System.out.println(arg[0]);
+		System.out.println(arg[1]);
+		System.out.println(z);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
